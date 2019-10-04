4c62f6842de2164902d51a118763c46c2571ca13
s3667779 <s3667779@student.rmit.edu.au>
2019-10-04 17:04:19 +1000

    Add tests for login

--- frontend/social-app/src/Tests/Login.test.js
+++ frontend/social-app/src/Tests/Login.test.js
@@ -1,3 +1,28 @@
-test('false test', () =>{
-    expect(false).toBeFalsy();
+import React from 'react';
+import {render, unmountComponentAtNode} from "react-dom";
+import {act} from "react-dom/test-utils";
+import {shallow, mount} from "enzyme";
+import LoginComponent from "../../../todo-app/src/components/todo/LoginComponent";
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+describe("<LoginScreen/>", () => {
+    it('renders without crashing', () => {
+        act(() => {
+            render(<LoginComponent/>, container);
+        });
+        expect(container).toMatchSnapshot();
+    });
 });
