9e7deb1d1eec8220c238fa913d74c8f49ef4b702
Jonathan Diver <s3707275@student.rmit.edu.au>
2019-10-04 16:09:18 +1000

    Verbose jest, plus additional dependencies for tsx

--- frontend/social-app/src/Tests/EventItem.test.js
+++ frontend/social-app/src/Tests/EventItem.test.js
@@ -1,3 +1,11 @@
+// import {CreateEvent} from '../Containers/CreateEvent'
+// import { shallow } from 'enzyme';
+
+
 test('false test', () =>{
     expect(false).toBeFalsy();
 });
+
+// test('create event correctly rendered', () => {
+//     const event = shallow(<CreateEvent></CreateEvent>);
+// });
