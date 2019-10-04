80fe169befd51cdcf8999aec9733245647c7fb12
Sheryl Mantik <s3693591@student.rmit.edu.au>
2019-10-04 15:53:15 +1000

    attempt on creating tests

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/JwtUserInMemoryTests.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/JwtUserInMemoryTests.java
@@ -1,4 +1,24 @@
 package com.sept.rest.webservices.restfulwebservices;
 
-import org.junit.*;
+import com.sept.rest.webservices.restfulwebservices.login.StudentRepository;
+import com.sept.rest.webservices.restfulwebservices.jwt.JwtInMemoryUserDetailsService;
 
+import org.junit.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JwtUserInMemoryTests {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void checkUsername() throws UsernameNotFoundException {
+        
+    }
+
+
+
+}
