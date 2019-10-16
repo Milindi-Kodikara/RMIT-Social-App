43df2de794e9e4ee158655dcafb35cb489dea164
Sheryl Mantik <s3693591@student.rmit.edu.au>
2019-10-16 15:22:24 +1100

    show all list working on the backend side, front end couldn't show up

--- frontend/social-app/src/Containers/EventList.tsx
+++ frontend/social-app/src/Containers/EventList.tsx
@@ -14,13 +14,13 @@ interface EventListState {
     events: {
         id: string;
         username: string,
-        imgURL: string,
-        startTime: Date;
-        finishTime: Date;
         name: string;
-        description: string;
         location: string;
+        startTime: Date;
+        endTime: Date;
         organiser: string;
+        description: string;
+        imgURL: string,
     }[],
     isLoading: boolean
 }
@@ -49,62 +49,87 @@ class EventList extends Component<EventListProps, EventListState> {
 
     render() {
         
-        const organisers = {
-            tpc: 'The Programming Club',
-            csit: 'CSIT Society'
-        }
+        const {isLoading, events} = this.state
 
-        let events = [
-            {
-                id: 'abcde',
-                username: 'sept',
-                name: 'Intro to SE ',
-                startTime: new Date(),
-                finishTime: new Date(),
-                location: '80.5.10',
-                organiser: organisers.tpc,
-                description: 'Softwares are cool . . apparently',
-                imgURL: 'https://sportslinkt-images.s3-ap-southeast-2.amazonaws.com/profile_410_600.jpg'
-            }, {
-                id: 'abcdf',
-                username: 'sept',
-                startTime: new Date(),
-                finishTime: new Date(),
-                name: 'Struc. Prog.',
-                location: '12.10.11',
-                organiser: organisers.csit,
-                description: 'Make Tim W. proud. P.s python sucks ><',
-                imgURL: 'https://sportslinkt-images.s3-ap-southeast-2.amazonaws.com/profile_410_600.jpg'
-            }, {
-                id: 'abcdg',
-                username: 'sept',
-                startTime: new Date(1),
-                finishTime: new Date(),
-                name: "Mili is da best!!!!",
-                location: '80.06.11',
-                organiser: organisers.tpc,
-                description: 'Story of an amazing hooman ~>,<~ ',
-                imgURL: 'https://sportslinkt-images.s3-ap-southeast-2.amazonaws.com/profile_410_600.jpg'
-            }
-        ]
-        return (<div>
-                {events.map((event) => <EventItem
+        return (<div>
+            {!isLoading ? (
+                events.map((event) =>
+                    <EventItem
                     key={event.id}
                     id={event.id}
                     username={event.username}
                     name={event.name}
-                    image={<img className="image" src={event.imgURL}/>}
+                    image={<img className="image" src={event.imgURL}/>}
                     location={event.location}
                     startTime={event.startTime}
-                    finishTime={event.finishTime}
+                    endTime={event.endTime}
                     organiser={event.organiser}
                     description={event.description}
                     imgURL={event.imgURL}
                     colour={"#FF0001"}
-                />)}
-
+                    //clubName={"TPC"}
+            />)) : <p>Loading ...</p>}
+                    
             </div>
         )
+
+        // const organisers = {
+        //     tpc: 'The Programming Club',
+        //     csit: 'CSIT Society'
+        // }
+
+        // let events = [
+        //     {
+        //         id: 'abcde',
+        //         username: 'sept',
+        //         name: 'Intro to SE ',
+        //         startTime: new Date(),
+        //         finishTime: new Date(),
+        //         location: '80.5.10',
+        //         organiser: organisers.tpc,
+        //         description: 'Softwares are cool . . apparently',
+        //         imgURL: 'https://sportslinkt-images.s3-ap-southeast-2.amazonaws.com/profile_410_600.jpg'
+        //     }, {
+        //         id: 'abcdf',
+        //         username: 'sept',
+        //         startTime: new Date(),
+        //         finishTime: new Date(),
+        //         name: 'Struc. Prog.',
+        //         location: '12.10.11',
+        //         organiser: organisers.csit,
+        //         description: 'Make Tim W. proud. P.s python sucks ><',
+        //         imgURL: 'https://sportslinkt-images.s3-ap-southeast-2.amazonaws.com/profile_410_600.jpg'
+        //     }, {
+        //         id: 'abcdg',
+        //         username: 'sept',
+        //         startTime: new Date(1),
+        //         finishTime: new Date(),
+        //         name: "Mili is da best!!!!",
+        //         location: '80.06.11',
+        //         organiser: organisers.tpc,
+        //         description: 'Story of an amazing hooman ~>,<~ ',
+        //         imgURL: 'https://sportslinkt-images.s3-ap-southeast-2.amazonaws.com/profile_410_600.jpg'
+        //     }
+        // ]
+
+        // return (<div>
+        //         {events.map((event) => <EventItem
+        //             key={event.id}
+        //             id={event.id}
+        //             username={event.username}
+        //             name={event.name}
+        //             image={<img className="image" src={event.imgURL}/>}
+        //             location={event.location}
+        //             startTime={event.startTime}
+        //             finishTime={event.finishTime}
+        //             organiser={event.organiser}
+        //             description={event.description}
+        //             imgURL={event.imgURL}
+        //             colour={"#FF0001"}
+        //         />)}
+
+        //     </div>
+        // )
     }
 }
 
